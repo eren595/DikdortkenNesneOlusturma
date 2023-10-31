663fa757672be9b74101e46a2660e57e2cd67fbb
Muhammederen7 <muhammederenyorulmaz7@gmail.com>
2023-10-31 11:55:30 +0300

    Proje dosyası ekle.

---
+++ DikdortkenNesneOlusturma/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DikdortkenNesneOlusturma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen(13, 40);
+            Console.WriteLine("Dikdörtgenin Alanı="+dikdortgen.AlanHesapla());
+            Console.WriteLine("Dikdörtgenin Çevresi="+dikdortgen.CevreHesapla());
+        }
+    }
+    class Dikdortgen
+    {
+        private int kisaKenar, uzunKenar;
+        public Dikdortgen(int kisaKenar, int uzunKenar)
+        {
+            this.kisaKenar = kisaKenar;
+            this.uzunKenar = uzunKenar;
+        }
+        public int AlanHesapla()
+        {
+            return kisaKenar * uzunKenar;
+        }
+        public int CevreHesapla()
+        {
+            return 2 * (kisaKenar + uzunKenar);
+        }
+    }
+
+}
